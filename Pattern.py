a1f95d69d85ff733fc82c9a930c2a3f3c5267136
Safiuddin <149352127+MajorMunna@users.noreply.github.com>
2025-05-21 19:20:15 +0600

    Print the pattern by for loop
    Print the pattern (for/while):
    1
    22
    333
    4444
    55555

---
+++ Pattern.py
@@ -0,0 +1,4 @@
+for i in range(1, 6): # Loop from 1 to 5 (inclusive)
+        # The number of times 'i' is repeated is 'i' itself
+        # Convert 'i' to string to allow repetition
+        print(str(i) * i)
